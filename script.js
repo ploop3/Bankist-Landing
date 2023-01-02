19d044be0c911c7663d2f9975ad043174397a44b
Pedro <28835813+ploop3@users.noreply.github.com>
2023-01-02 17:51:34 +0100

    implement smooth scroll into view

--- script.js
+++ script.js
@@ -29,3 +29,11 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+//Scroll Learn more button to Section 1
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function(e){
+  section1.scrollIntoView({behavior:'smooth'});
+});
