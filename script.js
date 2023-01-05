355ca84d02543da60a79eae89c92d8aad6af481a
Pedro <28835813+ploop3@users.noreply.github.com>
2023-01-05 21:28:09 +0100

    Tabbed component

--- script.js
+++ script.js
@@ -37,3 +37,34 @@ const section1 = document.querySelector('#section--1');
 btnScrollTo.addEventListener('click', function(e){
   section1.scrollIntoView({behavior:'smooth'});
 });
+
+
+/* 
+Tabbed Component
+Select the active tab and content by simply removing and adding classes
+*/
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContents = document.querySelectorAll('.operations__content');
+
+//Use event delegation to listen only on the parent of all three buttons
+tabsContainer.addEventListener('click', function(e){
+  //.closest in case we select the <span> child of the button
+  const clicked = e.target.closest('.operations__tab');
+
+  //Guard clause (if we click inside the container but not any button)
+  if(!clicked) return;
+
+  //Active tabs
+    //1. Deactivate all tabs and contents
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContents.forEach(c => c.classList.remove('operations__content--active'));
+
+
+    //2. Activate the clicked tab and content using the data id
+  clicked.classList.add('operations__tab--active');
+
+  document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active');
+
+
+});
